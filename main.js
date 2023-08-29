46d54c9db02d95ef5620cff2153a25c51989f8fe
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-08-29 00:21:45 -0300

    Finished loan display and display with a link

--- main.js
+++ main.js
@@ -1,33 +1,63 @@
-const initialTerms = ["Hello", "Goodbye", "Good", "I want"]
+// Elements
 const chat = document.getElementById("chat-historic")
 const input = document.getElementById("message-input")
 const btnSend = document.getElementById("btn-submit")
 
-btnSend.addEventListener("click", function () {
-  const messageUser = input.value.trim() // trim() - clears whitespace from string.
+// Initial terms recognized by the bot
+const initialTerms = ["hello", "goodbye", "good", "i want"]
+
+// Loan options and information
+const loanOptions = [
+  {
+    option: "Do you want to apply for a loan?",
+    info: 'To apply for a loan, please visit our loan application <a href="https://www.itau.com.br/" target="_blank">page</a>.',
+  },
+  {
+    option: "Loan conditions",
+    info: 'Our loan <a href="https://emprestimo.itau.com.br/emprestimo-consignado/proposta" target="_blank">conditions</a> include low interest rates.',
+  },
+  {
+    option: "Help",
+    info: 'If you need assistance regarding loans, please <a href="https://www.itau.com.br/atendimento-itau/para-voce" target="_blank">contact</a> our support team.',
+  },
+]
+
+// Event listener for submit button
+btnSend.addEventListener("click", handleSendMessage)
+
+function handleSendMessage() {
+  const messageUser =
+    input.value.trim() /* trim - remove whitespace from the beginning and end of the string */
   if (messageUser !== "") {
     addMessage("user", messageUser)
 
-    const termFound = initialTerms.find((term) =>
-      messageUser.toLowerCase().includes(term.toLowerCase())
-    )
+    const lowerCaseMessage =
+      messageUser.toLowerCase() /* Converts user message to lower case */
 
+    const termFound = findMatchingTerm(lowerCaseMessage)
     if (termFound) {
-      conversation(termFound)
+      initiateConversation(termFound)
+    } else if (lowerCaseMessage.includes("loan")) {
+      initiateConversation("loan")
     } else {
       addMessage("bot", "Sorry! I didn't understand. Can you repeat, please?")
     }
+
     input.value = ""
   }
-})
-
-function conversation(term) {
+}
+/* Responsible for starting a chat conversation based on the term given as an argument */
+function initiateConversation(term) {
   const response = getResponse(term)
   addMessage("bot", response)
 }
-
+/* Search for a matching term in the initial keywords (initialTerms) based on the message given as an argument*/
+function findMatchingTerm(message) {
+  return initialTerms.find((term) => message.includes(term))
+}
+/* Responsible for returning the appropriate response based on the term provided */
 function getResponse(term) {
-  switch (term.toLowerCase()) {
+  switch (term) {
     case "hello":
       return "Hello! How can I help?"
     case "goodbye":
@@ -36,22 +66,41 @@ function getResponse(term) {
       return "I'm glad this helped you!"
     case "i want":
       return "What do you want?"
+    case "loan":
+      const loanOptionsHTML = loanOptions
+        .map(
+          (option, index) =>
+            `<div class="loan-option bot-message" data-option="${index}">${option.option}</div>`
+        )
+        .join("")
+      return loanOptionsHTML
+    default:
+      return "Sorry, I didn't understand!"
   }
 }
 
+/* Responsible for adding messages to the chat history displayed in the interface */
 function addMessage(sender, message) {
   const messageElement = document.createElement("div")
   messageElement.className = sender === "user" ? "user-message" : "bot-message"
-  messageElement.textContent = message
+  messageElement.innerHTML = message
   chat.appendChild(messageElement)
   chat.scrollTop = chat.scrollHeight
 }
 
-function btnmessage() {
-  let message = document.getElementById("message-input")
-  if (!message.value) {
-    message.style.border = "1px solid red"
-  } else {
-    message.style.border = "none"
-  }
+/* Responsible for updating the message input element's border style based on its content */
+function updateMessageInputStyle() {
+  const message = document.getElementById("message-input")
+  message.style.border = !message.value ? "2px solid red" : "none"
 }
+
+/* Used to interact with the loan options presented in the chat */
+document.addEventListener("click", function (event) {
+  if (event.target.classList.contains("loan-option")) {
+    const selectedOptionIndex = event.target.getAttribute("data-option")
+    const selectedOption = loanOptions[selectedOptionIndex]
+    addMessage("user", selectedOption.option)
+    addMessage("bot", selectedOption.info)
+    input.value = ""
+  }
+})

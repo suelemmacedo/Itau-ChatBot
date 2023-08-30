2871fd7f80bded3092d771090d86c8231e10dab1
Suelem Macedo <suelemmacedo_@hotmail.com>
2023-08-30 19:38:28 -0300

    Finished ChatBot

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Elements
+/* Elements */
 const chat = document.getElementById("chat-historic")
 const input = document.getElementById("message-input")
 const btnSend = document.getElementById("btn-submit")
 
-// Initial terms recognized by the bot
+/* Initial terms recognized by the bot */
 const initialTerms = ["hello", "goodbye", "good", "i want"]
 
-// Loan options and information
+/* Loan options and information */
 const loanOptions = [
   {
     option: "Do you want to apply for a loan?",
@@ -21,38 +21,65 @@ const loanOptions = [
     info: 'If you need assistance regarding loans, please <a href="https://www.itau.com.br/atendimento-itau/para-voce" target="_blank">contact</a> our support team.',
   },
 ]
+/* Check if the user is authenticated */
+let isAuthenticated = false
 
-// Event listener for submit button
+/* Function to prompt user for username and password */
+function promptForCredentials() {
+  const userName = prompt("Enter your username:")
+  const password = prompt("Enter your password:")
+
+  /* Check if the entered credentials are correct */
+  if (userName === "user1" && password === "12345") {
+    isAuthenticated = true
+    addMessage("bot", "Authentication successful. How can I assist you today?")
+  } else {
+    addMessage("bot", "Authentication failed. Please try again.")
+  }
+}
+
+/* Event listener for submit button */
 btnSend.addEventListener("click", handleSendMessage)
 
 function handleSendMessage() {
-  const messageUser =
-    input.value.trim() /* trim - remove whitespace from the beginning and end of the string */
+  // Check if the user is authenticated before processing the message
+  if (!isAuthenticated) {
+    promptForCredentials()
+    return
+  }
+  const messageUser = input.value.trim()
   if (messageUser !== "") {
+    const lowerCaseMessage = messageUser.toLowerCase()
+
     addMessage("user", messageUser)
+    addMessageToHistory("user", messageUser)
 
-    const lowerCaseMessage =
-      messageUser.toLowerCase() /* Converts user message to lower case */
-
-    const termFound = findMatchingTerm(lowerCaseMessage)
+    const termFound = matchTerm(lowerCaseMessage)
     if (termFound) {
       initiateConversation(termFound)
     } else if (lowerCaseMessage.includes("loan")) {
       initiateConversation("loan")
     } else {
       addMessage("bot", "Sorry! I didn't understand. Can you repeat, please?")
+      addMessageToHistory(
+        "bot",
+        "Sorry! I didn't understand. Can you repeat, please?"
+      )
     }
 
     input.value = ""
   }
 }
+
 /* Responsible for starting a chat conversation based on the term given as an argument */
 function initiateConversation(term) {
   const response = getResponse(term)
   addMessage("bot", response)
+  addMessageToHistory("bot", response)
 }
+
 /* Search for a matching term in the initial keywords (initialTerms) based on the message given as an argument*/
-function findMatchingTerm(message) {
+function matchTerm(message) {
   return initialTerms.find((term) => message.includes(term))
 }
 /* Responsible for returning the appropriate response based on the term provided */
@@ -85,7 +112,8 @@ function addMessage(sender, message) {
   messageElement.className = sender === "user" ? "user-message" : "bot-message"
   messageElement.innerHTML = message
   chat.appendChild(messageElement)
-  chat.scrollTop = chat.scrollHeight
+  chat.scrollTop =
+    chat.scrollHeight /* Adjusted to show the most recent message */
 }
 
 /* Responsible for updating the message input element's border style based on its content */
@@ -101,6 +129,60 @@ document.addEventListener("click", function (event) {
     const selectedOption = loanOptions[selectedOptionIndex]
     addMessage("user", selectedOption.option)
     addMessage("bot", selectedOption.info)
-    input.value = ""
+    addMessageToHistory("user", selectedOption.option) // Add user message to history
+    addMessageToHistory("bot", selectedOption.info) // Add bot message to history
+    input.value = "" // Clear input
   }
 })
+
+/* Framework for storing conversation message history */
+const conversationHistory = []
+
+function addMessageToHistory(sender, message) {
+  const timestamp = new Date()
+  const messageObject = {
+    sender: sender,
+    message: message,
+    timestamp: timestamp,
+  }
+  conversationHistory.push(messageObject)
+}
+
+// Export conversation history
+const exportButton = document.getElementById("export-button")
+
+exportButton.addEventListener("click", function () {
+  const csvContent = prepareCSV(conversationHistory)
+  downloadCSV(csvContent, "conversation_history.csv")
+})
+
+/* Responsible for formatting the conversation history in a CSV(Comma-Separated Values) format */
+function prepareCSV(history) {
+  let csv = "Sender,Message,Timestamp\n"
+
+  history.forEach((entry) => {
+    const formattedTimestamp = entry.timestamp.toLocaleString()
+    csv += `${entry.sender},"${entry.message}",${formattedTimestamp}\n`
+  })
+
+  return csv
+}
+
+/* Responsible for creating a download link to a CSV file and allowing the user to download that file */
+function downloadCSV(csvContent, filename) {
+  const blob = new Blob([csvContent], {
+    type: "text/csv;charset=utf-8;",
+  }) /* Blob is a data structure representing raw binary data */
+  const url = URL.createObjectURL(blob)
+
+  const link = document.createElement("a")
+  link.href = url
+  link.setAttribute("download", filename)
+  link.style.display = "none"
+
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link)
+
+  URL.revokeObjectURL(url)
+}
